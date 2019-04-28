f9fd536ecaa4865dea7f978ab82bcadd5526f911
theodora-roseti <theodoraroseti@gmail.com>
2019-04-28 23:26:11 +0300

    SRAN-9 : user entity

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\UserEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,4 +26,31 @@ class AdminController extends AbstractAppController {
 			'admin/admin.html.twig'
 		);
 	}
+    /**
+     * @Route("/users", name="admin_users", methods={"GET","POST"})
+     * @return Response
+     */
+    public function usersAction(UserEntityRepository $userEntityRepository, Request $request, EntityManagerInterface $entityManager): Response {
+        if($this->shouldRedirectUser()){
+            return $this->redirectToCorrectPage();
+        }
+        foreach ($request->get("normalUsers",[]) as $user){
+            $dbUser = $userEntityRepository->find($user);
+            $dbUser->setIsAdmin(true);
+            $entityManager->persist($dbUser);
+        }
+        foreach ($request->get("adminUsers",[]) as $user){
+            $dbUser = $userEntityRepository->find($user);
+            $dbUser->setIsAdmin(false);
+            $entityManager->persist($dbUser);
+        }
+        $entityManager->flush();
+        return $this->render(
+            'admin/users.html.twig',
+            ["normalUsers" => $userEntityRepository->findBy(["isAdmin"=>0]),
+                "adminUsers" => $userEntityRepository->findBy(["isAdmin"=>1])
+        ]
+
+        );
+    }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+
+class UserType extends AbstractType
+{
+
+}
