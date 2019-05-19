5876704cb78330793722de0ea043612772525bf8
Alexandru Botici <alexbotici@gmail.com>
2019-05-19 23:23:22 +0300

    SRAN-24 : Device filter on table page

--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -u www-data sran-apache-server sh -c "composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader && composer dump-env prod && npm install && npm run compile-scss && php bin/console doctrine:migrations:migrate --no-interaction"
+docker exec -u www-data sran-apache-server sh -c "git reset --hard origin/master 2>&1 ; npm ci 2>&1 ; npm run compile-scss 2>&1 ; php bin/console doctrine:migrations:migrate --no-interaction ; composer dump-env prod 2>&1 ; composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader 2>&1"
